dcdee8799813adc8bdb5201252ee801481da978f
vyku visvalingm <vykunthan.visvalingam@spsmail.cuny.edu>
2014-09-07 22:33:11 -0500

    Example on Python Variables

---
+++ eg_variables.py
@@ -0,0 +1,15 @@
+'''My First Simple Program
+Date : 7-Sep-2014
+Author: Vykunthan
+'''
+print "Vykunthan First Python Program"
+a = 8
+b = 2
+print ("Addition:%d"% (a+b));
+print ("Subtraction:%d"% (a-b));
+print ("Multiply:%d"% (a*b));
+print ("Division:%d"% (a/b));
+
+firstName = "Sathvika"
+lastName = "Vaikunth"
+print ("Full Name :"+ (firstName + " " + lastName));
